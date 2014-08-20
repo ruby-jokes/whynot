7e841d476106c0ad027501ddca1216dbadbece7c
José Tomás Albornoz <jojo@eljojo.net>
2014-08-20 12:09:37 +0200

    All methods can (or maybe not) take a block

--- spec/whynot_spec.rb
+++ spec/whynot_spec.rb
@@ -1,8 +1,49 @@
 require 'spec_helper'
 
 describe 'meh' do
-
   it "should be true or false" do
     expect(meh.to_s).to match(/true|false/)
+  end
+
+  it "takes an optional block" do
+    expect(
+      meh { :yay }.to_s
+    ).to match(/^$|yay/)
+  end
+end
+
+describe 'maybe' do
+  it "should be true or false" do
+    expect(maybe.to_s).to match(/true|false/)
+  end
+
+  it "takes an optional block" do
+    expect(
+      maybe { :yay }.to_s
+    ).to match(/^$|yay/)
+  end
+end
+
+describe 'mostly' do
+  it "should be true or false" do
+    expect(mostly.to_s).to match(/true|false/)
+  end
+
+  it "takes an optional block" do
+    expect(
+      mostly { :yay }.to_s
+    ).to match(/^$|yay/)
+  end
+end
+
+describe 'occasionally' do
+  it "should be true or false" do
+    expect(occasionally.to_s).to match(/true|false/)
+  end
+
+  it "takes an optional block" do
+    expect(
+      occasionally { :yay }.to_s
+    ).to match(/^$|yay/)
   end
 end
